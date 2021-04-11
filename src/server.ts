e29305a3c17d860e3c1a26a9df9c306da4e6fd27
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-11 23:14:29 +0700

    a

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
-
+// import * as PostRouter from './routes/post';
+// import * as AuthRouter from './routes/auth';
+import * as mongoose from 'mongoose';
+const db = mongoose.connection;
 const app = express();
 
-app.use(bodyParser.json());
+let MongoDB_URI =
+  process.env.mongoDB_URI ||
+  'mongodb://127.0.0.1:27017/mts-technonatura-server';
+mongoose.connect(MongoDB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(express.json());
+
+// db.on('error', (err) => console.error('error when connecting to db'));
+// db.once('open', () => console.log('connected to mongoose'));
+
+// app.use('/', PostRouter);
+// app.use('/', AuthRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: 'hey' });
 });
-
 app.listen(process.env.PORT || 3000, () => {
   console.log(`server started on port ${process.env.PORT}`);
 });
