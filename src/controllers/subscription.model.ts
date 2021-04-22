c48c4a62424c08ac0b5ecdaec72e07d7cf761568
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-22 19:45:29 +0700

    subcription model

---
+++ src/controllers/subscription.model.ts
@@ -0,0 +1,51 @@
+import { Schema, Model, Document, model, Types, Query } from 'mongoose';
+import * as Validator from 'validator';
+import * as bcrypt from 'bcrypt';
+
+const {
+  default: { isEmail, isURL },
+} = Validator;
+
+export interface SubscriptionInterface {
+  email: string;
+  name: string;
+}
+
+export interface SubscriptionBaseDocument
+  extends SubscriptionInterface,
+    Document {}
+
+// Export this for strong typing
+export interface SubscriptionDocument extends SubscriptionBaseDocument {}
+
+// For model
+export interface SubscriptionModel extends Model<SubscriptionBaseDocument> {}
+
+const SubscriptionSchema = new Schema<SubscriptionDocument, SubscriptionModel>({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    lowercase: true,
+    unique: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  name: {
+    type: String,
+    lowercase: true,
+    required: [true, 'Subscriptionname cannot be blank'],
+    validate: [validateUsername, 'Only Letters and Numbers are allowed'],
+  },
+});
+
+function validateUsername(str: string) {
+  if (!str.match(/^[a-zA-Z0-9]+$/)) {
+    return false;
+  }
+
+  return true;
+}
+
+export default model<SubscriptionDocument, SubscriptionModel>(
+  'Subscription',
+  SubscriptionSchema,
+);
