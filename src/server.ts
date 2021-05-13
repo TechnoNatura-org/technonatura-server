a424685489ca2dba5cf65182ecee60366e52fc5f
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-05-13 09:51:03 +0700

    modify request header on global

--- src/server.ts
+++ src/server.ts
@@ -31,11 +31,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
+
   next();
 });
 
