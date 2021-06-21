ef51c145a1824b334a3ff0bf45d849145aab5a90
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-06-21 15:49:06 +0700

    a

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -8,29 +8,67 @@
  */
 
 import { gql } from 'apollo-server-express';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-const Global = fs.readFileSync(`${__dirname}/index.graphql`, 'utf8');
+// const Global = fs.readFileSync(`${__dirname}/index.graphql`, 'utf8');
 
-const ArduinoAppSensor = fs.readFileSync(
-  `${__dirname}/arduinoapp/sensor.graphql`,
-  'utf8',
-);
-const ArduinoApp = fs.readFileSync(
-  `${__dirname}/arduinoapp/arduinoApp.graphql`,
-  'utf8',
-);
-const ArduinoAppGlobal = fs.readFileSync(
-  `${__dirname}/arduinoapp/index.graphql`,
-  'utf8',
-);
+// const ArduinoAppSensor = fs.readFileSync(
+//   `${__dirname}/arduinoapp/sensor.graphql`,
+//   'utf8',
+// );
+// const ArduinoApp = fs.readFileSync(
+//   `${__dirname}/arduinoapp/arduinoApp.graphql`,
+//   'utf8',
+// );
+// const ArduinoAppGlobal = fs.readFileSync(
+//   `${__dirname}/arduinoapp/index.graphql`,
+//   'utf8',
+// );
 
 export default gql`
-  ${Global}
+  interface APIResponse {
+    message: String
+    status: String
+  }
 
-  ${ArduinoAppSensor}
-  ${ArduinoApp}
-  ${ArduinoAppGlobal}
+  type ArduinoSensorData {
+    _id: ID!
+    data: Int
+    date: Int
+  }
+
+  type ArduinoSensor {
+    _id: ID!
+    name: String!
+    own: String!
+    appID: String!
+  }
+  type ArduinoApp {
+    _id: ID
+    name: String!
+    desc: String!
+    own: String!
+    token: ArduinoAppToken
+    sensors: [String]
+    subscribe: Boolean!
+  }
+  # arduino app
+  type ArduinoAppToken {
+    token: String!
+    tokenCreated: Int!
+  }
+
+  type GetArduinoAppResponse implements APIResponse {
+    message: String
+    status: String
+    app: ArduinoApp
+  }
+
+  type GetArduinoAppSensorResponse implements APIResponse {
+    message: String
+    status: String
+    sensor: ArduinoSensor
+  }
 
   type Query {
     hello: String
