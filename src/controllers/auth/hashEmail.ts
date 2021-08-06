0524eb1b4fdb2a499cde01a87db41a6272bcbcb0
aldhanekaa <aldhanekadev@gmail.com>
2021-08-06 22:51:50 +0700

    a

--- src/controllers/auth/hashEmail.ts
+++ src/controllers/auth/hashEmail.ts
@@ -1,22 +1,22 @@
 import * as crypto from 'crypto';
-var algorithm = 'aes-192-cbc'; //algorithm to use
-var password = process.env.HASH_EMAIL_SECRET_PASS;
+// var algorithm = 'aes-192-cbc'; //algorithm to use
+// var password = process.env.HASH_EMAIL_SECRET_PASS;
 
-const key = crypto.scryptSync(
-	// @ts-ignore
-	password,
-	process.env.HASH_EMAIL_SECRET_SALT_KEY,
-	24,
-); //create key
+// const key = crypto.scryptSync(
+// 	// @ts-ignore
+// 	password,
+// 	process.env.HASH_EMAIL_SECRET_SALT_KEY,
+// 	24,
+// ); //create key
 
-const iv = crypto.randomBytes(16); // generate different ciphertext everytime
-const cipher = crypto.createCipheriv(algorithm, key, iv);
-var encrypt = (text: string) =>
-	cipher.update(text, 'utf8', 'hex') + cipher.final('hex'); // encrypted text
+// const iv = crypto.randomBytes(16); // generate different ciphertext everytime
+// const cipher = crypto.createCipheriv(algorithm, key, iv);
+// var encrypt = (text: string) =>
+// 	cipher.update(text, 'utf8', 'hex') + cipher.final('hex'); // encrypted text
 
-const decipher = crypto.createDecipheriv(algorithm, key, iv);
+// const decipher = crypto.createDecipheriv(algorithm, key, iv);
 
-var decrypt = (encryptedEmail: string) =>
-	decipher.update(encryptedEmail, 'hex', 'utf8') + decipher.final('utf8'); //deciphered text
+// var decrypt = (encryptedEmail: string) =>
+// 	decipher.update(encryptedEmail, 'hex', 'utf8') + decipher.final('utf8'); //deciphered text
 
-export { encrypt, decrypt };
+// export { encrypt, decrypt };
