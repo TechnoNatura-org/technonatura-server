484cc43cd4c651210b2dcc3a4a905bb969641301
aldhanekaa <aldhanekadev@gmail.com>
2021-08-07 20:36:30 +0700

    modified cors config

--- src/controllers/cors.ts
+++ src/controllers/cors.ts
@@ -7,9 +7,8 @@ import * as cors from 'cors';
 // cors.
 var whitelist = [
 	'http://localhost:3000',
-	'https://mts-tn.vercel.app',
-	'https://mts-technonatura.vercel.app',
-	'https://dashboard.mts-technonatura.vercel.app',
+	'https://technonatura-dashboard.vercel.app/',
+	'https://dashboard.technonatura.vercel.app/',
 ];
 var corsOptions: cors.CorsOptions = {
 	origin: function(origin, callback) {
