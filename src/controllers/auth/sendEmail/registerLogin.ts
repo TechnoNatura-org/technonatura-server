ba6e65a1aee0a4c58258825e07adc4b999bd2727
aldhanekaa <aldhanekadev@gmail.com>
2021-08-11 05:38:32 +0700

    e

--- src/controllers/auth/sendEmail/registerLogin.ts
+++ src/controllers/auth/sendEmail/registerLogin.ts
@@ -17,10 +17,10 @@ export default async function sendRegisterInfo(
 	try {
 		let sendEmailRes = await transporter.sendMail({
 			from: '"Aldhaneka<DO NOT REPLY>" <aldhanekadev@gmail.com>', // sender address
-			to: email, // list of receivers
+			to: 'aldhaneka@gmail.com', // list of receivers
 			subject: 'Hi',
 			html: EmailTemplate(`
-  <body style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border: 0;" bgcolor="#ffffff"><table class="body" valign="top" role="presentation" border="0" cellpadding="0" cellspacing="0" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border: 0;" bgcolor="#ffffff">
+  <body >
 <h1>test 123</h1>
     </body>
     `),
